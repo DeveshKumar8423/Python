13ead79828a02729cf0ae5a4d394bb3035718639
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-02-01 22:34:52 +0530

    nested if

--- p.py
+++ p.py
@@ -1,3 +1,9 @@
-set = {"asmit", "devesh", "shubham"}
-set.remove("devesh")
-print(set)
+height = int(input("Enter your height in cm : "))
+age = int(input("Enter your age : "))
+if(height>=120):
+    if(age>=18):
+        print("You can ride")
+    else:
+        print("You cannot ride")
+else:
+    print("You cannot ride")
