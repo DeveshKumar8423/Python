675fb34e38caea7b17eb87dfdd4a9759c9115a3a
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-02-07 11:26:26 +0530

    Convert name into title

---
+++ zt.py
@@ -0,0 +1,11 @@
+#Convert name into title
+def title_name(fname , lname):
+    f_name = fname.title()
+    l_name = lname.title()
+
+    print(f"{f_name} {l_name}")
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+
+title_name(first_name, last_name)
